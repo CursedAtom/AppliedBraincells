72e17a6d3e2803a8580431bee16e9be3f413cfcc
CursedAtom <88254538+CursedAtom@users.noreply.github.com>
2023-10-18 15:43:23 -0400

    Add Kubejs

---
+++ overrides/kubejs/server_scripts/recipes/mekanism/combining/gravel.js
@@ -0,0 +1,20 @@
+ServerEvents.recipes(event => {
+    event.custom({
+      {
+         "type": "mekanism:combining",
+         "extraInput": {
+           "ingredient": {
+             "tag": "forge:cobblestone/normal"
+           }
+         },
+         "mainInput": {
+           "ingredient": {
+             "item": "minecraft:flint"
+           }
+         },
+         "output": {
+           "item": "minecraft:gravel"
+         }
+       }
+       })
+})
